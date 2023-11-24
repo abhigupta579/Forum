06e7dd6be02246354b8b9a6cd0fb896d67cb74e8
Abhi Gupta <abhigupta579@gmail.com>
2023-11-24 17:39:44 +0530

    learn2

---
+++ footer.php
@@ -0,0 +1,3 @@
+<div class="mt-4 mb-0 bg-dark container-fluid text-white">
+    <p class="text-center">Copyrights reserved 2023 iDiscuss Forums.</p>
+</div>
--- index.php
+++ index.php
@@ -6,12 +6,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forum</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
 </head>
 
 <body>
-    <h1></h1>
+    <?php
+    require 'header.php';
+    ?>
+    <div class="container">
+        <h2 class="text-center">iDiscuss Categories : </h2>
+        <div class="row">
+            <!-- Use a For Loop to iterate... -->
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://source.unsplash.com/500x400/?car,audi" class="card-img-top" alt="...">
+                    <!-- We use unsplash Apis... -->
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">View Threads</a>
+                    </div>
+                </div>
+            </div>
 
+        </div>
+    </div>
+
+    <?php require 'footer.php'; ?>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
